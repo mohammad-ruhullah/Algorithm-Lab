75a8d17a382186790c37718ed86c4194cf365051
Mohammad Ruhullah <c201048@ugrad.iiuc.ac.bd>
2022-06-26 22:13:14 +0600

    Add files via upload

---
+++ fractionalKnapsak.cpp
@@ -0,0 +1,68 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+bool comp(pair<double, double>x, pair<double, double> y){
+	return x.first/x.second > y.first/y.second;
+}
+
+int main(){
+
+
+	cout << "Enter the number of item: ";
+	int n;
+	cin >> n;
+
+	cout << "Enter Maximum Size of Kanpsak : ";
+	int m;
+	cin >> m;
+
+	cout << "Enter the Profit and Weight of each Item : " << endl;
+
+	vector<pair<double, double>> item(n);
+	for(int i = 0; i < n; i++){
+		cin >> item[i].first >> item[i].second;
+	}
+
+	sort(item.begin(), item.end(), comp);
+
+
+	double profit = 0;
+
+	for(int i = 0; i < n; i++){
+		
+		if(item[i].second <= m){
+			m = m - item[i].second;
+			profit = profit + item[i].first;
+		}else{
+			if(m == 0)break;
+			profit = profit + (item[i].first / item[i].second) * m;
+			break;
+		}
+	}
+
+
+	cout << profit << endl;
+
+
+
+
+
+	return 0;
+}
+
+
+// 3
+// 50
+// 60 10
+// 100 20
+// 120 30
+// 240
+
+// 4
+// 60
+// 120 24
+// 280 40
+// 100 10
+// 120 20
+//440
+
